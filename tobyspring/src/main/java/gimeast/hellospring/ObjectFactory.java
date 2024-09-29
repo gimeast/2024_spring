8c9b75478644505610512d1427132975ffba4531
gimeast <dudu.gimeast@gmail.com>
2024-09-29 19:51:24 +0900

    토비의 스프링 6 - 이해와 원리
    - 스프링 컨테이너와 의존관계 주입

--- tobyspring/src/main/java/gimeast/hellospring/ObjectFactory.java
+++ tobyspring/src/main/java/gimeast/hellospring/ObjectFactory.java
@@ -1,12 +1,19 @@
 package gimeast.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
+//        return new SimpleExRateProvider();
         return new WebApiExRateProvider();
     }
 }
