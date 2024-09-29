309e1a9585dffeb1461dd6c0ffa99f442330a98b
gimeast <dudu.gimeast@gmail.com>
2024-09-29 21:31:57 +0900

    토비의 스프링 6 - 이해와 원리
    - 구성정보를 가져오는 다른 방법

--- tobyspring/src/main/java/gimeast/hellospring/ObjectFactory.java
+++ tobyspring/src/main/java/gimeast/hellospring/ObjectFactory.java
@@ -1,11 +1,12 @@
 package gimeast.hellospring;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-
+/*
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
@@ -16,4 +17,5 @@ public class ObjectFactory {
 //        return new SimpleExRateProvider();
         return new WebApiExRateProvider();
     }
+*/
 }
