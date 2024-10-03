b76eaa986e87e826e61d01a75d235d0e17cbb498
gimeast <dudu.gimeast@gmail.com>
2024-10-04 03:44:45 +0900

    토비의 스프링 6 - 이해와 원리
    - ApiTemplate 분리

--- tobyspring/src/main/java/gimeast/hellospring/exrate/WebApiExRateProvider.java
+++ tobyspring/src/main/java/gimeast/hellospring/exrate/WebApiExRateProvider.java
@@ -1,45 +1,21 @@
 package gimeast.hellospring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import gimeast.hellospring.api.ApiExecutor;
+import gimeast.hellospring.api.ApiTemplate;
 import gimeast.hellospring.api.ErApiExRateExtractor;
-import gimeast.hellospring.api.ExRateExtractor;
-import gimeast.hellospring.api.SimpleApiExecutor;
+import gimeast.hellospring.api.HttpClientApiExecutor;
 import gimeast.hellospring.payment.ExRateProvider;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class WebApiExRateProvider implements ExRateProvider {
+    ApiTemplate apiTemplate = new ApiTemplate();
 
     @Override
     public BigDecimal getExchangeRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
-        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
+
+        return apiTemplate.getExRate(
+                url, new HttpClientApiExecutor(), new ErApiExRateExtractor()
+        );
     }
-
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return exRateExtractor.extractExRate(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
